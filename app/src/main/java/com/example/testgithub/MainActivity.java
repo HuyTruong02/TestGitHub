4c6deb540efb5351ad853fca3e9b254b7da5d313
truongpro <nguyenhuytruongit@gmail.com>
2022-11-09 09:12:50 +0700

    truong dang nhap

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-Button btn;
+Button btn; TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +16,8 @@ Button btn;
     }
 
     public void anhxa(){
+        textView = findViewById(R.id.textView);
         btn = findViewById(R.id.btn);
+
     }
 }
